96092db6ec1e555b64f6506c6b8bd1d6e023bb80
Tikev39 <tikev39@hotmail.fr>
2022-08-27 10:36:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bd4rdu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bd4rdu)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { produits } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  produits = produits; // Avoir accès à la liste des produits à partir de product-list.html. On la déclare ici
+
+  share() { // Méthode permettant d'afficher un pop up indiquant que le produit a été partagé
+    window.alert('Le produit a été partagé avec succès!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
