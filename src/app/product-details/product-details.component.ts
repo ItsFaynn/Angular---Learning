73dd0c6e7c5bb2486fbe2e0c14ff128a488beb29
Tikev39 <tikev39@hotmail.fr>
2022-08-29 08:49:09 -0400

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Produit, produits } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { } // Injection de ActiviatedRoute dans le constructeur. Route privé en tant qu'argument
+
+  produit: Produit | undefined // Définition de la propriété produit
 
   ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('produuitId'));
+  
+    // Find the product that correspond with the id provided in route.
+    this.produit = produits.find(produits => produits.id === productIdFromRoute);
   }
 
 }
+
+
+// The route parameters correspond to the path variables you define in the route. To access the route parameters, we use route.snapshot, which is the ActivatedRouteSnapshot that contains information about the active route at that particular moment in time. The URL that matches the route provides the productId . Angular uses the productId to display the details for each unique product.
